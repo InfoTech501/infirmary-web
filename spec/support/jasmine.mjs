dc98f81f522086118cc04a75bd516a09a5986504
jakeeviado <jakematthewviado204@gmail.com>
2025-09-04 18:06:15 +0800

    added ci pipeline and dependency for automating build and test using jasmine

---
+++ spec/support/jasmine.mjs
@@ -0,0 +1,17 @@
+// Jasmine configuration for running tests generated using : npx jasmine init
+
+
+export default {
+  spec_dir: "spec",
+  spec_files: [
+    "**/*[sS]pec.?(m)js"
+  ],
+  helpers: [
+    "helpers/**/*.?(m)js"
+  ],
+  env: {
+    stopSpecOnExpectationFailure: false,
+    random: true,
+    forbidDuplicateNames: true
+  }
+}
