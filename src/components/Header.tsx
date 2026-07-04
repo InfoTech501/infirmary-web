fa0ba824afc3a014eef250e8de3f36b2e08d2c9c
M-Anacay <mary.ann.jacinto.anacay@gmail.com>
2026-07-05 07:26:49 +0800

    feat: adviser dashboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,31 @@
-import React from "react"
+"use client"
+
+import { useEffect, useState } from "react"
 import { Bell, UserCircle2Icon } from "lucide-react"
 import Link from "next/link"
-
-const adviserName = "Mrs. Jean C. Dela Cruz"
+import { getCurrentUser } from "@/services/authService"
 
 export function Header() {
+  const [currentUser, setCurrentUser] = useState<any>(null)
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        // Paste token here
+        const token =
+          "token"
+
+        const user = await getCurrentUser(token);
+
+        setCurrentUser(user);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchCurrentUser()
+  }, [])
+
   return (
     <nav className="fixed top-0 left-60 right-0 bg-white px-6 py-4 flex items-center justify-between">
       <h1 className="text-3xl font-bold text-blue-950">Dashboard</h1>
@@ -21,10 +42,16 @@ export function Header() {
           className="flex items-center gap-2 hover:bg-gray-200 rounded-full px-2 py-1"
         >
           <UserCircle2Icon size={30} />
-          <div>
-            <p className="text-sm font-medium">{adviserName}</p>
-            <p className="text-xs text-gray-500">Adviser</p>
-          </div>
+          {currentUser && (
+              <div>
+              <p className="text-sm font-medium">
+                Ms. {currentUser.employee.person.firstName}{" "} 
+                {currentUser.employee.person.middleName}{" "}
+                {currentUser.employee.person.lastName}
+              </p>
+              <p className="text-xs text-gray-500">Adviser</p>
+              </div>
+            )}
         </Link>
       </div>
     </nav>
